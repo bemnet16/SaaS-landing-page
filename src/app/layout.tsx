ba006a4eaba86c71ba07dcf500fed29c23ae0137
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-09-06 16:45:04 +0300

    add favicon icon
    add favicon icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="relative">
+      <head>
+       <link rel="icon" href="/favicon.png" />
+      </head>
       <body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE]")}>
         {children}
       </body>
